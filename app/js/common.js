561a00c9e5c78f44a1b7a8003757fb050b637104
Viktor Hameza <viktor.hameza@gmail.com>
2016-04-19 14:28:35 +0300

    test

--- app/js/common.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $(window).load(function() {
-    $('.flexslider').flexslider({
-      animation: "slide",
-      controlNav: false,
-      // directionNav: false,
-    });
-  });
-});
